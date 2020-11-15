5d2bc130f307f692f43b0bf15e199f04a14642e5
Cesar Montenegro <montenegroback@gmail.com>
2020-11-15 14:48:57 -0400

    [ADDED]: Dependencies element-ui and chart

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Element from "element-ui";
+import locale from "element-ui/lib/locale/lang/es";
+import "element-ui/lib/theme-chalk/index.css";
 
 Vue.config.productionTip = false
+
+// Element ui
+Vue.use(Element, { locale });
 
 new Vue({
   render: h => h(App),
