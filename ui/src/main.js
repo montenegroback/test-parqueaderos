355c7d9cba9dbe5e37b7f3857628c189cf7907ec
Cesar Montenegro <montenegroback@gmail.com>
2020-11-16 17:54:05 -0400

    [ADDED]: Dependency vue-code-highlight

--- ui/src/main.js
+++ ui/src/main.js
@@ -3,11 +3,16 @@ import App from './App.vue'
 import Element from "element-ui";
 import locale from "element-ui/lib/locale/lang/es";
 import "element-ui/lib/theme-chalk/index.css";
+import VueCodeHighlight from 'vue-code-highlight';
+import "vue-code-highlight/themes/window.css";
+
 
 Vue.config.productionTip = false
 
 // Element ui
 Vue.use(Element, { locale });
+
+Vue.use(VueCodeHighlight);
 
 new Vue({
   render: h => h(App),
