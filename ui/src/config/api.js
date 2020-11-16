465508f00721b36189cef6fdf73813bd1b7170e2
Cesar Montenegro <montenegroback@gmail.com>
2020-11-16 09:56:12 -0400

    [FIT]: URL api ui

--- ui/src/config/api.js
+++ ui/src/config/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default () => {
   return axios.create({
-    baseURL: `http://127.0.0.1:8000/api/`,
+    baseURL: `https://api-parqueaderon4.herokuapp.com/api/`,
     withCredentials: false,
     headers: {
       Accept: "application/json",
